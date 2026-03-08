a63849ab8911973829ed22e2646d84eebea39bd0
Nana Andoh <nanaegyir2000@hotmail.com>
2026-03-08 17:30:44 -0600

    considering using pandas instead

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from openpyxl import load_workbook
 from openpyxl.worksheet.table import Table, TableStyleInfo
 import os
 import sys
-
+from openpyxl.worksheet.filters import (
+    FilterColumn,
+    CustomFilter,
+    CustomFilters,
+    DateGroupItem,
+    Filters,
+    BlankFilter
+    )
 #potential for command line arguments that take in media directory and excel file
 def main():
 
@@ -14,6 +21,18 @@ def main():
     wb = load_workbook(filename=excel_file)
     ws = wb.active
     tb = ws.tables['Table2']
+    # once we have the table, we'll filter out empty loaded dates and unload dates with none
+    
+    blank = BlankFilter()
+    
+    # filter for rows that have not been dev'd yet. this is how we'll control which folders are made
+
+    # start at folder photography/2026/film then direct to the correct folder for the camera or make folder for camera if it does not exist
+
+    # folder naming convention is Film brand, film stock, roll start date in YYYY-MM-DD, roll end date
+
+
+    print(tb)
     return
 
 
